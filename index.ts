328fcad0cda3f2d0769d0991f8c694cc68c7b0de
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-17 09:35:58 +0530

    added cors

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 
 import express, { Express, Request, Response, Application } from 'express';
 import { connectDB } from './db/conn';
-import {router as eventRoutes} from './routes/events';
-
+import { router as eventRoutes } from './routes/events';
+import cors from 'cors';
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript Server');
 });
