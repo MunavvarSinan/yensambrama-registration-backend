1fe7fc82e5f235b6ec72ae5585e0780a4509d6c6
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-17 09:46:37 +0530

    added cors

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const conn_1 = require("./db/conn");
 const events_1 = require("./routes/events");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
+// Define your frontend domain
+const allowedOrigins = ['http://localhost:3000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+app.use((0, cors_1.default)(corsOptions));
 app.get('/', (req, res) => {
     res.send('Welcome to Express & TypeScript Server');
+});
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 (0, conn_1.connectDB)();
 app.use(express_1.default.json());
