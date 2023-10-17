fc4d4ae805d1e675118b374ca07b9f0fa8cdbb14
MunavvarSinan <munavvarsinan8@gmail.com>
2023-10-17 09:41:49 +0530

    added cors

--- index.ts
+++ index.ts
@@ -11,6 +11,12 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript Server');
 });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
